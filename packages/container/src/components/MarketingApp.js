7c29453d7aa3050d96a98f5f4dd5dffd30d967ec
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-03 16:29:25 +0800

    first commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+const MarketingApp = () => {
+  // generic logic
+  const ref = useRef(null);
+  useEffect(() => {
+    mount(ref.current);
+  });
+  return <div ref={ref} />;
+};
+
+export default MarketingApp;
