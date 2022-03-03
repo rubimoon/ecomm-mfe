c54b37dec0abad20baec61bb47fcecc9af9e0fa6
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-03 22:08:35 +0800

    configure public path

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,6 +6,7 @@ const packageJson = require('../package.json');
 const prodConfig = {
   mode: 'production',
   output: {
+    publicPath: '/marketing/latest/',
     filename: '[name].[contenthash].js',
   },
   plugins: [
