6286410316d26b5bceec3689a5c6461204f77268
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-04 11:26:02 +0800

    add event listener

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -8,7 +8,7 @@ const MarketingApp = () => {
   const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathname) {
@@ -16,7 +16,9 @@ const MarketingApp = () => {
         }
       },
     });
-  });
+    history.listen(onParentNavigate);
+  }, []);
+
   return <div ref={ref} />;
 };
 
