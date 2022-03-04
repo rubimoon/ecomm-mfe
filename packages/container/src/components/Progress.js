5fbbbd980a9d9b8c198d10f42b857020e990c775
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-04 15:32:06 +0800

    fix import reference

--- packages/container/src/components/Progress.js
+++ packages/container/src/components/Progress.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import { LinearProgress } from '../../../marketing/node_modules/@material-ui/core';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => {
   return createStyles({
