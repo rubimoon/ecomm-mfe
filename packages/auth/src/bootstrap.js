dc4c6f72670b432ef19af2279fd566b55a41bef4
Ruby Chang <dev.rubimoon@gmail.com>
2022-03-04 13:51:18 +0800

    initialize auth project

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+// function invoked in container to start up the app
+// default history is only gonna be provided in dev env
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  // whenever the path(url) changes
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+      if (nextPathname !== pathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
